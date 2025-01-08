13fb4fd13e8ac26a105ea8626ae84a915014bc53
Ayon9 <65212004+Ayon9@users.noreply.github.com>
2025-01-09 02:36:59 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,92 @@
+hablu=420
+print(type(hablu))
+gablu = 4.20
+print(type(gablu))
+kodu = 5058j
+print(type(kodu))
+print(200000)
+print("hello britney")
+hello="my name is"
+print(hello+ " " +'ayon')
+by="i"
+byby="how r u"
+print(by+ " " +byby)
+x=100
+y=100
+print(x/y)
+num1 = 10
+num2 = 500
+print("my name is ayon {num1+num2}")
+hablu=[1,5,2,0,5,8,2,2,4,1,1,4,1,5,5]
+b=bytes(hablu)
+print(type(b))
+hablu=[1,5,1,5,5,5,5,2,85,0,5,25,8,25,8,52,2]
+b1=bytearray(hablu)
+print(type(b1))
+b1[1]=100
+print(b1[1])
+list=['ayon', 'sakib', 'udoy', 'niloy']
+list[1]='hablu'
+print(list)
+list1=('ayon','sakib','udoy','niloy')
+print(type(list1))
+print(list1)
+tup=(15,55,55,52,52,55,0)
+print(tup)
+print(type(tup))
+ran=range(6)
+print(ran)
+#Arithmetic oprator
+# Addition	
+a=10058
+b=25505
+print(a+b)
+# substraction
+a=55220
+b=54555
+print(a-b)
+# multification
+a=55252
+b=5555
+print(a*b) 
+# Division	
+a=555
+b=232
+print(a/b)
+# Modulus	
+a=500
+b=350
+print(a%b)
+# Exponentiation
+a=10
+b=3
+print(a ** b)
+# Floor division	
+a=15
+b=2
+print(a // b)
+# Assignment operators
+b=50
+sum=b+50
+print(sum)
+a=50
+a+=50
+print(a)
+# swapping oprator
+a=100
+b=90
+a,b=b,a
+print(a)
+print(b)
+# User Input
+username = input("enter your name:")
+password = input("enter your password:")
+print(username)
+print(password)
+# casting type
+name = "12850"
+print(int(name))
+print(type(int(name)))
+newnum="50"
+float(newnum)
+print(float(newnum))
